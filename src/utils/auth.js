6121d0626d926311e02599488942985ff1ae4a79
Pernell Bauza <pernellbauza@gmail.com>
2024-01-23 20:52:47 -0800

    auth js added

---
+++ src/utils/auth.js
@@ -0,0 +1,21 @@
+import { BASE_URL, handleServerResponse } from "./api";
+
+export const register = ({ name, avatar, email, password }) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar, email, password }),
+  }).then(handleServerResponse);
+};
+
+export const login = ({ email, password }) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(handleServerResponse);
+};
